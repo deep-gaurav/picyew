ab3c70b85cc858d802d8f7e945b577af0e131de6
Deep <deepgauravraj@gmail.com>
2020-07-20 19:17:13 +0530

    remove node

--- copyfilestodist.sh
+++
@@ -1,5 +0,0 @@
-rm -rf dist
-mkdir -p dist
-cp ./static/* dist/
-cp ./pkg/bundle.js dist/
-cp ./pkg/package_bg.wasm dist/
---
+++ vercel.sh
@@ -0,0 +1,20 @@
+set -x
+curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain stable -y
+source ~/.cargo/env
+
+curl -L https://github.com/rustwasm/wasm-pack/releases/download/v0.9.1/wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz --output wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
+
+ls -l
+tar -zxvf wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
+
+export PATH="$PATH:$PWD/wasm-pack-v0.9.1-x86_64-unknown-linux-musl"
+
+npm install -g rollup
+
+wasm-pack build --target web --out-name package
+rollup ./main.js --format iife --file ./pkg/bundle.js
+
+mkdir -p dist
+cp static/* dist/
+cp pkg/bundle.js dist/
+cp pkg/package_bg.wasm dist/
